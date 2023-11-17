3e4e35f47942aaf780ff8b844c029c06b1fdfb50
Istia Budi <istiabudi@gmail.com>
2023-11-17 14:31:50 +0800

    fix little bug

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -42,7 +42,7 @@ class CommentsHandler {
      * Untuk mendapatkan useCase, pastikan Anda memanfaatkan method `this._container.getInstance`
      */
     const useCase = this._container.getInstance(DeleteCommentUseCase.name);
-    await useCase.execute({ owner, threadId, commentId });
+    await useCase.execute({ owner, threadId, id });
 
     return {
       status: 'success',
